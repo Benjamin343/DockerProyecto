85acb0a86aeca4a314ccd01c38566bbac86743d4
Benjamin Oyarzo <114113432+Benjamin343@users.noreply.github.com>
2023-08-06 04:36:07 -0400

    Add files via upload

---
+++ Frontend/vecired-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  //url: 'http://192.168.1.107:3000'
+  //url: 'http://34.176.5.244:443'
+  //url: 'http://localhost:3000'
+  //url: 'https://shrouded-reaches-97156.herokuapp.com'
+  //url: 'https://vecired.herokuapp.com'
+<<<<<<< HEAD
+  url: 'http://localhost:3000'
+  //url: 'http://192.168.1.82:3000'
+=======
+  //url: 'http://localhost:3000'
+  //url: 'http://192.168.1.82:6000'
+    url: 'http://146.83.198.35:1481'
+>>>>>>> 699caa9c4692ccda4c02e6093c250767cf4b0177
+};
