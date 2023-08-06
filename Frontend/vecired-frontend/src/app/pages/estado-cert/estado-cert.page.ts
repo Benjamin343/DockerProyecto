6f8cdcd9eee0cd57a561b22c9f1dcae0bdc8faf1
Benjamin Oyarzo <114113432+Benjamin343@users.noreply.github.com>
2023-08-06 04:43:47 -0400

    Add files via upload

---
+++ Frontend/vecired-frontend/src/app/pages/estado-cert/estado-cert.page.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { EmisorService } from 'src/app/servicios/emisor.service';
+import { Emisor } from 'src/app/interfaces/interfaces';
+import { AlertasService } from 'src/app/servicios/alertas.service';
+
+@Component({
+  selector: 'app-estado-cert',
+  templateUrl: './estado-cert.page.html',
+  styleUrls: ['./estado-cert.page.scss'],
+})
+export class EstadoCertPage implements OnInit {
+  emisores: Emisor[] = [];
+  emptySolicitud = false;
+  mipagina= 1;
+  finInfinito =false;
+  
+  constructor(private emisorService: EmisorService,
+              private alertaService: AlertasService
+              ) { }
+
+  ngOnInit() {
+   // this.emisorService.getmisEmisores().subscribe( resp =>{
+    //  console.log(resp);
+    //});
+    //this.emisores = [];
+    this.getmisEmisores();
+
+  }
+
+  getmisEmisores(event?, pull: boolean = false) {
+    if (pull) {
+      this.emisores = [];
+      this.mipagina = 1;
+    }
+
+    this.emisorService.getmisEmisores(this.mipagina).subscribe((respuesta) => {
+      const nuevosEmisores = respuesta['emisor'];
+
+      if (pull) {
+        this.emisores = nuevosEmisores;
+      } else {
+        this.emisores.push(...nuevosEmisores);
+      }
+
+      this.emptySolicitud = this.emisores.length === 0;
+      this.mipagina++;
+      if (event) {
+        event.target.complete();
+      }
+    });
+  }
+
+  pull2refresh(event) {
+   // console.log('Pull to refresh event triggered.');
+    this.getmisEmisores(event, true);
+  }
+
+  infinito(event) {
+    this.getmisEmisores(event);
+  }
+
+  evento(){
+    this.alertaService.alerta('Esta esta funcionalidad aún no se encuentra desarrollada');
+  }
+
+}
---
+++ Frontend/vecired-frontend/src/app/pages/gest-cert/gest-cert.page.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit } from '@angular/core';
+import { Emisor } from 'src/app/interfaces/interfaces';
+import { EmisorService } from 'src/app/servicios/emisor.service';
+import { AlertasService } from '../../servicios/alertas.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-gest-cert',
+  templateUrl: './gest-cert.page.html',
+  styleUrls: ['./gest-cert.page.scss'],
+})
+export class GestCertPage implements OnInit {
+  emisores: Emisor[] = [];
+  emptySolicitud = false;
+  infiniteScroll = true;
+  pagina = 1;
+
+  aceptar: any;
+  rechazar: any;
+
+  constructor(private emisorService: EmisorService,
+              private alertaService: AlertasService
+              ) {}
+
+  ngOnInit() {
+    this.emisores = [];
+    this.obtenerSolicitudes();
+    //console.log(this.emisores.length);
+  }
+  obtenerSolicitudes(event?) {
+    this.emisorService.getEmisor(this.pagina).subscribe(
+      (respuesta) => {
+        const nuevosEmisores = respuesta['emisor'];
+
+        if (nuevosEmisores.length === 0) {
+          // se apaga el infinite scroll
+          this.infiniteScroll = false;
+          if (this.pagina === 1) {
+            // Si la página es 1 y no hay solicitudes, se muestra que la comunidad no tiene más solicitudes
+            this.emptySolicitud = true;
+          }
+          if (event) {
+            event.target.complete();
+          }
+          return;
+        }
+
+        this.emisores = this.emisores.concat(nuevosEmisores);
+        this.pagina++; // Increment the page number for infinite scroll.
+
+        if (event) {
+          event.target.complete();
+        }
+      },
+      (error) => {
+        console.error('Error al obtener las solicitudes:', error);
+        if (event) {
+          event.target.complete();
+        }
+      }
+    );
+  }
+
+  cargarNuevos(event) {
+    this.infiniteScroll = true;
+    this.obtenerSolicitudes(event);
+  }
+
+  pull2refresh(event) {
+    window.location.reload();
+  }
+
+
+  aprobarSolicitud(idSolicitud: string) {
+    //console.log('vamos a aprobar la siguiente solicitud ' + idSolicitud);
+
+    this.alertaService.alertaDecision('¿Desea aceptar a este usuario?').then(
+      respuesta => {
+        this.aceptar = respuesta['data'];
+        if (this.aceptar === true) {
+          this.emisorService.aceptarSolicitud(idSolicitud).subscribe(
+            (respuesta) => {
+             // console.log(respuesta);
+              
+              window.location.reload();
+            },
+            (error) => {
+              console.error('Error al aprobar la solicitud: ', error);
+            }
+          );
+        } else {
+          this.alertaService.presentToast('Operación cancelada');
+        }
+      }
+    );
+  }
+
+  rechazarSolicitud(idSolicitud: string) {
+    //console.log('vamos a rechazar la siguiente solicitud ' + idSolicitud);
+
+    this.alertaService.alertaDecision('¿Desea rechazar a este usuario?').then(
+      respuesta => {
+        this.aceptar = respuesta['data'];
+        if (this.aceptar === true) {
+          this.emisorService.rechazarSolicitud(idSolicitud).subscribe(
+            (respuesta) => {
+             // console.log(respuesta);
+
+              window.location.reload();
+            },
+            (error) => {
+              console.error('Error al rechazar la solicitud: ', error);
+            }
+          );
+        } else {
+          this.alertaService.presentToast('Operación cancelada');
+        }
+      }
+    );
+  }
+
+}
